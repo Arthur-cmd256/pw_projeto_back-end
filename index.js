b71b9a893b7a57a88c7a1ec84e9d6d35ed6fc736
Arthur <arthur.nascimento2020@outlook.com>
2022-06-19 13:06:56 -0300

    criando o arquivo Procfile para o heroku conseguir iniciar a api

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 require('dotenv').config()
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
+
+app.use(cors());
 
 const {
 
